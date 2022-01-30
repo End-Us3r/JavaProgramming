20a1cf68d2d79c1d924bc5c618a2f6a6f71c6edb
James Atkins <89127977+atkinsj5352@users.noreply.github.com>
2022-01-29 21:24:21 -0500

    Add files via upload

---
+++ M2NBP1.java
@@ -0,0 +1,31 @@
+//M2NBP1
+//James Atkins
+//CIS-115-0902
+//02-04-2021
+
+package m2nbp1;
+
+/**
+ *
+ * @author james
+ */
+public class M2NBP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        String name1 = "James Atkins";
+        String name2 = "CIS-115-0902";
+        
+        System.out.print("My name is ");
+        System.out.println(name1);
+        System.out.print("This class is ");
+        System.out.println(name2);
+        System.out.println("I do not know much code");
+        
+       
+    }
+    
+}
