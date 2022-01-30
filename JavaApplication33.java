749fd4cdc5bae44c945fabec613e721d03045600
James Atkins <89127977+atkinsj5352@users.noreply.github.com>
2022-01-29 21:23:16 -0500

    Add files via upload

---
+++ JavaApplication33.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication33;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class JavaApplication33 {
+
+    Scanner in = new Scanner(System.in);
+    
+    
+    public static void main(String[] args) {
+        
+       random(1, 7);
+       
+    }
+    
+    /**
+     *
+     * @param number
+     * @param number
+     * @return
+     */
+    public static double random(1, 7){
+       int count;
+      
+        for(count = 1; count <= 3;count++){
+           System.out.println(Math.random());
+            
+        }
+    return number;
+    
+    
+    
+  
+ }
+     
+     
+}
