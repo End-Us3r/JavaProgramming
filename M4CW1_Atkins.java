796de42278ec54c6742921e83ce069448df3e517
James Atkins <89127977+atkinsj5352@users.noreply.github.com>
2022-01-29 21:21:42 -0500

    Add files via upload

---
+++ M4CW1_Atkins.java
@@ -0,0 +1,29 @@
+//M4CW1
+//James Atkins
+//CIS-115-0902
+//02-25-2021
+package fieldtest;
+        
+        
+public class FieldTest 
+{
+    public static final int VALUE = 10;
+    
+    public static void main(String[] args) 
+    {
+        // Statments here have access to 
+        // the VALUE constant
+    }
+    
+    public static void method2()
+    {
+        // Statements here have access to
+        // the VALUE constant
+    }
+    
+    public static void method3() 
+    {
+        //Statments here have access to
+        // the VALUE constant
+    }
+}
