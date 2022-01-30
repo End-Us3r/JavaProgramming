d1f22ffe74004558787b0ff6d9459e165976f3ed
James Atkins <89127977+atkinsj5352@users.noreply.github.com>
2022-01-29 21:25:11 -0500

    Add files via upload

--- M4CW1_Atkins.java
+++ M4CW1_Atkins.java
@@ -1,29 +1,21 @@
-//M4CW1
-//James Atkins
-//CIS-115-0902
-//02-25-2021
-package fieldtest;
-        
-        
-public class FieldTest 
-{
-    public static final int VALUE = 10;
-    
-    public static void main(String[] args) 
-    {
-        // Statments here have access to 
-        // the VALUE constant
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m4cw1_atkins;
+
+/**
+ *
+ * @author james
+ */
+public class M4CW1_Atkins {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
     }
     
-    public static void method2()
-    {
-        // Statements here have access to
-        // the VALUE constant
-    }
-    
-    public static void method3() 
-    {
-        //Statments here have access to
-        // the VALUE constant
-    }
 }
