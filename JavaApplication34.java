c5c64913f78d9eb4df140390d99f439e5525aa09
James Atkins <89127977+atkinsj5352@users.noreply.github.com>
2022-01-29 21:23:46 -0500

    Add files via upload

---
+++ JavaApplication34.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication34;
+
+/**
+ *
+ * @author james
+ */
+public class JavaApplication34 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(random());
+        //int number = random();
+    }
+    
+    
+    public static int random(){
+        int number = random();
+        return number;
+    }
+}
